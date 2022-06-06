1bf9c1eb64c5526ca801eada58422f355cd8ec18
kairo <kairogyn@gmail.com>
2022-06-06 09:45:33 -0300

    pull generated code

--- src/pages/CreateAccount/styles.ts
+++ src/pages/CreateAccount/styles.ts
@@ -1,257 +1,258 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import {Form} from 'antd' 
-import GenericCodeButton from '../../components/Button'
-import Vector14840549090881Image from './assets/1521_133502.png' 
+import { Form } from 'antd';
+import GenericCodeButton from '../../components/Button';
+import Vector14840549090881Image from './assets/1521_133502.png';
+import GenericText from '../../components/Text';
 
 export const CreateAccount = styled.div`
-height: 500px;
-width: 800px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-justify-content: flex-start;
-align-items: flex-start;
-  box-shadow: 8px 8px 8px 0px rgba(0, 0, 0, 0.07999999821186066)  , 32px 32px 32px 0px rgba(0, 0, 0, 0.07999999821186066)  ,  64px 64px 64px 0px rgba(0, 0, 0, 0.05000000074505806)    ;
+  height: 500px;
+  width: 800px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: flex-start;
+  box-shadow: 8px 8px 8px 0px rgba(0, 0, 0, 0.07999999821186066),
+    32px 32px 32px 0px rgba(0, 0, 0, 0.07999999821186066),
+    64px 64px 64px 0px rgba(0, 0, 0, 0.05000000074505806);
 `;
 export const Login2 = styled.div`
-height: 500px;
-width: 400px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-justify-content: center;
-align-items: center;
-  padding: 48.0px 48.0px 48.0px 48.0px;
-    padding-top: 48.0px;
-    padding-left: 48.0px;
-    padding-bottom: 48.0px;
-    padding-right: 48.0px;
-  background-color: #FFFFFF;
+  height: 500px;
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: center;
+  padding: 48px 48px 48px 48px;
+  padding-top: 48px;
+  padding-left: 48px;
+  padding-bottom: 48px;
+  padding-right: 48px;
+  background-color: #ffffff;
 `;
 export const Content2 = styled.div`
-flex-direction: column;
-height: 244px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-row-gap: 8.0px;;
-column-gap: 8.0px;;
-justify-content: flex-start;
-align-items: center;
+  flex-direction: column;
+  height: 244px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  row-gap: 8px;
+  column-gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const Title1 = styled.div`
-height: 24px;
-width: 304px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-justify-content: center;
-align-items: flex-start;
+  height: 24px;
+  width: 304px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: flex-start;
 `;
-import GenericText from '../../components/Text'
 export const TextComponentcreating = styled(GenericText)`
-          font-weight: 500;
-          font-size: 16px;
-          color: #000000;
-          margin: 0px;
-        left: 30px;
-        width: 245px;
+  font-weight: 500;
+  font-size: 16px;
+  color: #000000;
+  margin: 0px;
+  left: 30px;
+  width: 245px;
 `;
 
 export const ViceriForm1 = styled(Form)`
-height: 172px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-row-gap: 8.0px;;
-column-gap: 8.0px;;
-justify-content: flex-start;
-align-items: flex-start;
-    cursor: pointer;
+  height: 172px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  row-gap: 8px;
+  column-gap: 8px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  cursor: pointer;
 `;
-export const InputFormItem2 = styled((Form.Item))`
-        width: 304px;
-      left: 0px;
-      top: 0px;
-      position: static;
+export const InputFormItem2 = styled(Form.Item)`
+  width: 304px;
+  left: 0px;
+  top: 0px;
+  position: static;
 `;
 
 export const InputLabelVertical2 = styled.div`
-height: 20px;
-width: 304px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-row-gap: 4.0px;;
-column-gap: 4.0px;;
-justify-content: flex-start;
-align-items: center;
+  height: 20px;
+  width: 304px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  row-gap: 4px;
+  column-gap: 4px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const InputLabel2 = styled(GenericText)`
-          font-size: 12px;
-          color: #000000;
-          margin: 0px;
-        width: 37px;
+  font-size: 12px;
+  color: #000000;
+  margin: 0px;
+  width: 37px;
 `;
 
-export const InputFormItem3 = styled((Form.Item))`
-      width: 304px;
-    left: 0px;
-    top: 60px;
-    position: static;
+export const InputFormItem3 = styled(Form.Item)`
+  width: 304px;
+  left: 0px;
+  top: 60px;
+  position: static;
 `;
 
 export const InputLabelVertical3 = styled.div`
-height: 20px;
-width: 304px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-row-gap: 4.0px;;
-column-gap: 4.0px;;
-justify-content: flex-start;
-align-items: center;
+  height: 20px;
+  width: 304px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  row-gap: 4px;
+  column-gap: 4px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const InputLabel3 = styled(GenericText)`
-        font-size: 12px;
-        color: #000000;
-        margin: 0px;
-      width: 57px;
+  font-size: 12px;
+  color: #000000;
+  margin: 0px;
+  width: 57px;
 `;
 
-export const InputFormItem4 = styled((Form.Item))`
-    width: 304px;
+export const InputFormItem4 = styled(Form.Item)`
+  width: 304px;
   left: 0px;
   top: 120px;
   position: static;
 `;
 
 export const InputLabelVertical4 = styled.div`
-height: 20px;
-width: 304px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-row-gap: 4.0px;;
-column-gap: 4.0px;;
-justify-content: flex-start;
-align-items: center;
+  height: 20px;
+  width: 304px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  row-gap: 4px;
+  column-gap: 4px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const InputLabel4 = styled(GenericText)`
-      font-size: 12px;
-      color: #000000;
-      margin: 0px;
-    width: 102px;
+  font-size: 12px;
+  color: #000000;
+  margin: 0px;
+  width: 102px;
 `;
 
 export const Actions2 = styled.div`
-height: 32px;
-width: 304px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-justify-content: flex-end;
-align-items: flex-start;
+  height: 32px;
+  width: 304px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: flex-end;
+  align-items: flex-start;
 `;
 export const ButtonsButton1 = styled(GenericCodeButton)`
-    left: 231px;
-      width: 73px;
-      height: 32px;
-      border-radius: 2px;
-      height: 32px;
+  left: 231px;
+  width: 73px;
+  height: 32px;
+  border-radius: 2px;
+  height: 32px;
 `;
 
 export const CreateaAccount1 = styled.div`
-height: 500px;
-width: 400px;
-display: flex;
-flex-direction: row;
-box-sizing: border-box;
-justify-content: center;
-align-items: flex-end;
-padding: 48.0px 48.0px 158.0px 48.0px;
-  padding-top: 48.0px;
-  padding-left: 48.0px;
-  padding-bottom: 158.0px;
-  padding-right: 48.0px;
-  background-color: #F7F1FF;
+  height: 500px;
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 48px 48px 158px 48px;
+  padding-top: 48px;
+  padding-left: 48px;
+  padding-bottom: 158px;
+  padding-right: 48px;
+  background-color: #f7f1ff;
 `;
 export const Content3 = styled.div`
-flex-direction: column;
-height: 182px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-row-gap: 40.0px;;
-column-gap: 40.0px;;
-justify-content: flex-start;
-align-items: flex-start;
+  flex-direction: column;
+  height: 182px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  row-gap: 40px;
+  column-gap: 40px;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const Logo1 = styled.div`
-flex-direction: column;
-height: 24px;
-width: 126px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-justify-content: flex-start;
-align-items: flex-start;
+  flex-direction: column;
+  height: 24px;
+  width: 126px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const Vector14840549090881 = styled.div`
-left: 0px;
-top: 0px;
-position: static;
-height: 24px;
-width: 126px;
-box-sizing: border-box;
+  left: 0px;
+  top: 0px;
+  position: static;
+  height: 24px;
+  width: 126px;
+  box-sizing: border-box;
   background: url(${Vector14840549090881Image});
   background-repeat: no-repeat;
 `;
 export const Text2 = styled.div`
-flex-direction: column;
-height: 46px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-justify-content: flex-start;
-align-items: flex-start;
+  flex-direction: column;
+  height: 46px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const Alreadyhaveanaccount = styled(GenericText)`
-      font-weight: 500;
-      font-size: 16px;
-      color: #000000;
-      margin: 0px;
-    width: 305px;
+  font-weight: 500;
+  font-size: 16px;
+  color: #000000;
+  margin: 0px;
+  width: 305px;
 `;
 
 export const Wearewaitingforyou = styled(GenericText)`
-    color: #000000;
-    margin: 0px;
+  color: #000000;
+  margin: 0px;
   top: 24px;
   width: 143px;
 `;
 
 export const Actions3 = styled.div`
-flex-direction: column;
-height: 32px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-row-gap: 8.0px;;
-column-gap: 8.0px;;
-justify-content: flex-start;
-align-items: flex-start;
+  flex-direction: column;
+  height: 32px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  row-gap: 8px;
+  column-gap: 8px;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const ButtonsButton2 = styled(GenericCodeButton)`
-    width: 67px;
-    height: 32px;
-    border-radius: 2px;
-    height: 32px;
+  width: 67px;
+  height: 32px;
+  border-radius: 2px;
+  height: 32px;
 `;
-
