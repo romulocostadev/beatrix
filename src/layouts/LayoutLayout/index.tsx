6eab04d51bc147b00daa00197e296159ca4e6153
kairo <kairogyn@gmail.com>
2022-06-07 10:15:19 -0300

    pull generated code

--- src/layouts/LayoutLayout/index.tsx
+++ src/layouts/LayoutLayout/index.tsx
@@ -2,33 +2,18 @@ import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 
-
-import {
-LayoutLayout,
-Content, Body,
-} from './styles';
+import { LayoutLayout, Content, Body } from './styles';
 
 const LayoutLayoutPage = () => {
+  return (
+    <LayoutLayout>
+      <Body>
+        <Content>
+          <Outlet />
+        </Content>
+      </Body>
+    </LayoutLayout>
+  );
+};
 
-
-
-
-
-
-
-
-
-return (
-        <LayoutLayout>
-<Body>
-<Content>
-<Outlet/>
-</Content>
-</Body>
-</LayoutLayout>
-
-    )
-
-}
-
-export default LayoutLayoutPage
+export default LayoutLayoutPage;
