9d337e27b7037662d101d565281c6adec143b2d5
romulocostadev <romulocostadev@hotmail.com>
2022-06-07 13:12:41 +0000

    Updating generated code

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import {Button} from 'antd';
+export const GenericButtonStyle = styled(Button)`
+width: 73px;
+position: static;
+left: 0px;
+top: 0px;
+`;
+
---
+++ src/layouts/LayoutLayout/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Outlet } from 'react-router-dom';
+
+
+import {
+LayoutLayout,
+Content, Body,
+} from './styles';
+
+const LayoutLayoutPage = () => {
+
+
+
+
+
+
+
+
+
+return (
+        <LayoutLayout>
+<Body>
+<Content>
+<Outlet/>
+</Content>
+</Body>
+</LayoutLayout>
+
+    )
+
+}
+
+export default LayoutLayoutPage
