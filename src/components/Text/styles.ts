6bbec0df00efc59d779635dba91003849cdf082c
romulocostadev <romulocostadev@hotmail.com>
2022-06-08 17:34:38 -0300

    Updating generated code

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
-import { Typography } from 'antd';
-
+import {Typography} from 'antd';
 export const GenericTextStyle = styled(Typography.Text)`
-  width: 27px;
-  position: static;
-  left: 21.5px;
-  top: 34px;
-  font-family: Roboto;
-  font-weight: 500;
-  font-size: 12px;
-  color: rgba(0, 0, 1, 1);
-  text-align: left;
+width: 27px;
+position: static;
+left: 21.5px;
+top: 34px;
+font-family: Roboto;
+font-weight: 500;
+font-size: 12px;
+color: rgba(0, 0, 1, 1) ;
+text-align: left;
 `;
+
--- src/layouts/components/LayoutHeader3/styles.ts
+++ src/layouts/components/LayoutHeader3/styles.ts
@@ -1,55 +1,56 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-import Vector14840549090881Image from './assets/1531_152486.png';
+import Vector14840549090881Image from './assets/1531_152486.png' 
 
 export const LayoutHeader1 = styled.div`
-  flex-direction: column;
-  height: 48px;
-  width: 1920px;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  justify-content: center;
-  align-items: center;
-  background-color: #ffffff;
+flex-direction: column;
+height: 48px;
+width: 1920px;
+display: flex;
+flex-direction: column;
+box-sizing: border-box;
+justify-content: center;
+align-items: center;
+  background-color: #FFFFFF;
 `;
 export const Header1 = styled.div`
-  height: 48px;
-  width: 1920px;
-  display: flex;
-  flex-direction: row;
-  box-sizing: border-box;
-  justify-content: flex-start;
-  align-items: center;
-  padding-right: 16px;
+height: 48px;
+width: 1920px;
+display: flex;
+flex-direction: row;
+box-sizing: border-box;
+justify-content: flex-start;
+align-items: center;
+    padding-right: 16.0px;
 `;
 export const Beatrix1 = styled.div`
-  flex-direction: column;
-  height: 48px;
-  width: 116px;
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  row-gap: 10px;
-  column-gap: 10px;
-  justify-content: flex-start;
-  align-items: flex-start;
-  padding: 16px 16px 16px 16px;
-  padding-top: 16px;
-  padding-left: 16px;
-  padding-bottom: 16px;
-  padding-right: 16px;
+flex-direction: column;
+height: 48px;
+width: 116px;
+display: flex;
+flex-direction: column;
+box-sizing: border-box;
+row-gap: 10.0px;;
+column-gap: 10.0px;;
+justify-content: flex-start;
+align-items: flex-start;
+    padding: 16.0px 16.0px 16.0px 16.0px;
+      padding-top: 16.0px;
+      padding-left: 16.0px;
+      padding-bottom: 16.0px;
+      padding-right: 16.0px;
 `;
 export const Vector14840549090881 = styled.div`
-  left: 16px;
-  top: 16px;
-  position: static;
-  height: 16px;
-  width: 84px;
-  box-sizing: border-box;
+left: 16px;
+top: 16px;
+position: static;
+height: 16px;
+width: 84px;
+box-sizing: border-box;
   background: url(${Vector14840549090881Image});
   background-repeat: no-repeat;
 `;
-export const Divider = styled.div`
-  border: 1px solid #000000;
+export const Divider = styled(.div)`
+  border: 1px solid #000000 ;
 `;
+
