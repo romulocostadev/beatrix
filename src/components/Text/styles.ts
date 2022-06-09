3a0ffefc70e9fc18de18b0d24b991a488f87ef5d
romulocostadev <romulocostadev@hotmail.com>
2022-06-09 16:41:03 -0300

    Updating generated code

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
-import { Typography } from 'antd';
-
+import {Typography} from 'antd';
 export const GenericTextStyle = styled(Typography.Text)`
-  width: 27px;
-  position: static;
-  left: 21.5px;
-  top: 34px;
-  font-family: Roboto;
-  font-weight: 500;
-  font-size: 12px;
-  color: #7447f9;
-  text-align: left;
+width: 138px;
+position: static;
+left: 0px;
+top: 0px;
+font-family: Roboto;
+font-weight: 400;
+font-size: 12px;
+color: rgba(0, 0, 0, 0.85) ;
+text-align: left;
 `;
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from '../layouts/Home1';
-import HomeContent from '../pages/HomeContent';
-import Application from '../layouts/Application';
-import ApplicationContent from '../pages/ApplicationContent';
-
+import { Routes, Route } from 'react-router-dom'
+import CanvasLayout from '../layouts/CanvasLayout';
+import DataCanvas from '../../src/pages/DataCanvas';
 const IndexRoutes = () => (
-  <Routes>
-    <Route path="" element={<Home />} />
-    <Route path="/home" element={<Home />}>
-      <Route index element={<HomeContent />} />
-      <Route path="home-content" element={<HomeContent />} />
-    </Route>
-    <Route path="/application" element={<Application />}>
-      <Route index element={<ApplicationContent />} />
-      <Route path="application-content" element={<ApplicationContent />} />
-    </Route>
-  </Routes>
+<Routes>
+<Route  path='' element={<CanvasLayout/>} />
+<Route  path='/canvas-layout' element={<CanvasLayout/>} >
+<Route index  element={<DataCanvas/>} />
+<Route  path='data-canvas' element={<DataCanvas/>} />
+<Route  path='data-canvas' element={<DataCanvas/>} />
+</Route>
+</Routes>
 );
 export default IndexRoutes;
