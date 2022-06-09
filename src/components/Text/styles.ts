f0bc04eaaaa998762d53cfcbcb0b369222af08a8
kairo <kairogyn@gmail.com>
2022-06-09 15:33:37 -0300

    pull generated code

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
-import {Typography} from 'antd';
-export const GenericTextStyle = styled(Typography.Text)`
-width: 27px;
-position: static;
-left: 21.5px;
-top: 34px;
-font-family: Roboto;
-font-weight: 500;
-font-size: 12px;
-color: #7447F9;
-text-align: left;
-`;
+import { Typography } from 'antd';
 
+export const GenericTextStyle = styled(Typography.Text)`
+  width: 27px;
+  position: static;
+  left: 21.5px;
+  top: 34px;
+  font-family: Roboto;
+  font-weight: 500;
+  font-size: 12px;
+  color: #7447f9;
+  text-align: left;
+`;
