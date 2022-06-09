829bdd4ceaf6355520ece26bf254bf5c1895ed98
kairo <kairogyn@gmail.com>
2022-06-09 16:42:07 -0300

    pull generated code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import CanvasLayout from '../layouts/CanvasLayout';
-import DataCanvas from '../../src/pages/DataCanvas';
+import DataCanvas from '../pages/DataCanvas';
+
 const IndexRoutes = () => (
-<Routes>
-<Route  path='' element={<CanvasLayout/>} />
-<Route  path='/canvas-layout' element={<CanvasLayout/>} >
-<Route index  element={<DataCanvas/>} />
-<Route  path='data-canvas' element={<DataCanvas/>} />
-<Route  path='data-canvas' element={<DataCanvas/>} />
-</Route>
-</Routes>
+  <Routes>
+    <Route path="" element={<CanvasLayout />} />
+    <Route path="/canvas-layout" element={<CanvasLayout />}>
+      <Route index element={<DataCanvas />} />
+      <Route path="data-canvas" element={<DataCanvas />} />
+      <Route path="data-canvas" element={<DataCanvas />} />
+    </Route>
+  </Routes>
 );
 export default IndexRoutes;
