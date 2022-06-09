12eaf167e5157851ae71a665f01c760211407f8e
kairo <kairogyn@gmail.com>
2022-06-09 12:13:54 -0300

    pull generated code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Home from '../layouts/Home';
-import HomeContent from '../../src/pages/HomeContent';
+import HomeContent from '../pages/HomeContent';
+
 const IndexRoutes = () => (
-<Routes>
-<Route  path='' element={<Home/>} />
-<Route  path='/home' element={<Home/>} >
-<Route index  element={<HomeContent/>} />
-<Route  path='home-content' element={<HomeContent/>} />
-</Route>
-</Routes>
+  <Routes>
+    <Route path="" element={<Home />} />
+    <Route path="/home" element={<Home />}>
+      <Route index element={<HomeContent />} />
+      <Route path="home-content" element={<HomeContent />} />
+    </Route>
+  </Routes>
 );
 export default IndexRoutes;
