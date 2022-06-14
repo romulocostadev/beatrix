ea37d7ebaaff150a7f08d2c4dd40a42a146f89b6
kairo <kairogyn@gmail.com>
2022-06-14 10:20:08 -0300

    pull generated code

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,125 +1,102 @@
 import React from 'react';
 
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Typography } from 'antd';
-import GenericInput from '../../components/Input' 
-
+import GenericInput from '../../components/Input';
 
 import {
-Login,
-Login1,
-Content,
-Title,
-Welcomeback,
-ViceriForm,
-InputFormItem,
-InputFormItem1,
-Actions,
-ButtonsButton2,
-Content1,
-Button,
-ButtonsButton3,
-Content2,
-Button1,
-CreateaAccount,
-Content3,
-Logo,
-Vector1484054909088,
-Text1,
-Comecreatethefuture,
-TextComponentjoin,
-Actions1,
-ButtonsButton4,
-Content4,
-Button2,
+  Login,
+  Login1,
+  Content,
+  Title,
+  Welcomeback,
+  ViceriForm,
+  InputFormItem,
+  InputFormItem1,
+  Actions,
+  ButtonsButton2,
+  Content1,
+  Button,
+  ButtonsButton3,
+  Content2,
+  Button1,
+  CreateaAccount,
+  Content3,
+  Logo,
+  Vector1484054909088,
+  Text1,
+  Comecreatethefuture,
+  TextComponentjoin,
+  Actions1,
+  ButtonsButton4,
+  Content4,
+  Button2,
 } from './styles';
 
 const LoginPage = () => {
+  let navigate = useNavigate();
 
+  const navigateToLogin = () => {
+    navigate('/login');
+  };
 
+  return (
+    <Login>
+      <Login1>
+        <Content>
+          <Title>
+            <Welcomeback>Welcome back!</Welcomeback>
+          </Title>
+          <ViceriForm layout="vertical" onClick={navigateToLogin}>
+            <InputFormItem
+              label={<Typography.Text ellipsis>E-mail</Typography.Text>}
+              name="email"
+            >
+              <GenericInput placeholder="E-mail" />
+            </InputFormItem>
+            <InputFormItem1
+              label={<Typography.Text ellipsis>Password</Typography.Text>}
+              name="password"
+            >
+              <GenericInput placeholder="E-mail" />
+            </InputFormItem1>
+          </ViceriForm>
+          <Actions>
+            <ButtonsButton2>
+              <Content1>
+                <Button>Recovery Password</Button>
+              </Content1>
+            </ButtonsButton2>
+            <ButtonsButton3>
+              <Content2>
+                <Button1>Login</Button1>
+              </Content2>
+            </ButtonsButton3>
+          </Actions>
+        </Content>
+      </Login1>
+      <CreateaAccount>
+        <Content3>
+          <Logo>
+            <Vector1484054909088 />
+          </Logo>
+          <Text1>
+            <Comecreatethefuture>Come create the future!</Comecreatethefuture>
+            <TextComponentjoin>
+              Join us creating your account.
+            </TextComponentjoin>
+          </Text1>
+          <Actions1>
+            <ButtonsButton4>
+              <Content4>
+                <Button2>Create</Button2>
+              </Content4>
+            </ButtonsButton4>
+          </Actions1>
+        </Content3>
+      </CreateaAccount>
+    </Login>
+  );
+};
 
-
-let navigate = useNavigate()
-
-const navigateToLogin = () => {navigate('/login')};
-
-
-
-
-return (
-        <Login    >
-  <Login1    >
-    <Content    >
-      <Title    >
-        <Welcomeback >
-          Welcome back!
-        </Welcomeback>
-      </Title>
-      <ViceriForm  layout='vertical' onClick = {navigateToLogin} >
-        <InputFormItem
-        label={<Typography.Text ellipsis>E-mail</Typography.Text>}
-        name='email'
-        >
-          <GenericInput
-        placeholder='E-mail'
-          />
-        </InputFormItem>
-        <InputFormItem1
-        label={<Typography.Text ellipsis>Password</Typography.Text>}
-        name='password'
-        >
-          <GenericInput
-        placeholder='E-mail'
-          />
-        </InputFormItem1>
-      </ViceriForm>
-      <Actions    >
-        <ButtonsButton2    >
-          <Content1    >
-            <Button >
-              Recovery Password
-            </Button>
-          </Content1>
-        </ButtonsButton2>
-        <ButtonsButton3    >
-          <Content2    >
-            <Button1 >
-              Login
-            </Button1>
-          </Content2>
-        </ButtonsButton3>
-      </Actions>
-    </Content>
-  </Login1>
-  <CreateaAccount    >
-    <Content3    >
-      <Logo    >
-        <Vector1484054909088    >
-        </Vector1484054909088>
-      </Logo>
-      <Text1    >
-        <Comecreatethefuture >
-          Come create the future!
-        </Comecreatethefuture>
-        <TextComponentjoin >
-          Join us creating your account.
-        </TextComponentjoin>
-      </Text1>
-      <Actions1    >
-        <ButtonsButton4    >
-          <Content4    >
-            <Button2 >
-              Create
-            </Button2>
-          </Content4>
-        </ButtonsButton4>
-      </Actions1>
-    </Content3>
-  </CreateaAccount>
-</Login>
-
-    )
-
-}
-
-export default LoginPage
+export default LoginPage;
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,259 +1,259 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import {Form} from 'antd' 
-import GenericFormItem from '../../components/FormItem'
-import Vector1484054909088Image from './assets/1521_130435.png' 
+import { Form } from 'antd';
+import GenericFormItem from '../../components/FormItem';
+import Vector1484054909088Image from './assets/1521_130435.png';
+import GenericText from '../../components/Text';
 
 export const Login = styled.div`
-height: 500px;
-width: 800px;
-display: flex;
-box-sizing: border-box;
-justify-content: flex-start;
-align-items: flex-start;
-  box-shadow: 8px 8px 8px 0px rgba(0, 0, 0, 0.07999999821186066)  , 32px 32px 32px 0px rgba(0, 0, 0, 0.07999999821186066)  ,  64px 64px 64px 0px rgba(0, 0, 0, 0.05000000074505806)    ;
+  height: 500px;
+  width: 800px;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: flex-start;
+  box-shadow: 8px 8px 8px 0px rgba(0, 0, 0, 0.07999999821186066),
+    32px 32px 32px 0px rgba(0, 0, 0, 0.07999999821186066),
+    64px 64px 64px 0px rgba(0, 0, 0, 0.05000000074505806);
 `;
 export const Login1 = styled.div`
-height: 500px;
-width: 400px;
-display: flex;
-box-sizing: border-box;
-justify-content: center;
-align-items: center;
-  padding: 48.0px;
-  background-color: #FFFFFF;
+  height: 500px;
+  width: 400px;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: center;
+  padding: 48px;
+  background-color: #ffffff;
 `;
 export const Content = styled.div`
-height: 184px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
+  height: 184px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const Title = styled.div`
-height: 24px;
-width: 304px;
-display: flex;
-box-sizing: border-box;
-justify-content: center;
-align-items: flex-start;
+  height: 24px;
+  width: 304px;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: flex-start;
 `;
-import GenericText from '../../components/Text'
 export const Welcomeback = styled(GenericText)`
-          font-weight: 500;
-          font-size: 16px;
-          margin: 0px;
-        width: 110px;
+  font-weight: 500;
+  font-size: 16px;
+  margin: 0px;
+  width: 110px;
 `;
 
 export const ViceriForm = styled(Form)`
-height: 112px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: flex-start;
-    cursor: pointer;
+  height: 112px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  cursor: pointer;
 `;
 export const InputFormItem = styled(GenericFormItem)`
-        width: 304px;
-      position: static;
+  width: 304px;
+  position: static;
 `;
 
 export const InputLabelVertical = styled.div`
-height: 20px;
-width: 304px;
-display: flex;
-box-sizing: border-box;
-gap: 4.0px;
-justify-content: flex-start;
-align-items: center;
+  height: 20px;
+  width: 304px;
+  display: flex;
+  box-sizing: border-box;
+  gap: 4px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const InputLabel = styled(GenericText)`
-          margin: 0px;
+  margin: 0px;
 `;
 
-export const InputFormItem1 = styled(GenericFormItem)`
-`;
+export const InputFormItem1 = styled(GenericFormItem)``;
 
 export const InputLabelVertical1 = styled.div`
-height: 20px;
-width: 304px;
-display: flex;
-box-sizing: border-box;
-gap: 4.0px;
-justify-content: flex-start;
-align-items: center;
+  height: 20px;
+  width: 304px;
+  display: flex;
+  box-sizing: border-box;
+  gap: 4px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const InputLabel1 = styled(GenericText)`
-        margin: 0px;
-      width: 57px;
+  margin: 0px;
+  width: 57px;
 `;
 
 export const Actions = styled.div`
-justify-content: space-between;
-height: 32px;
-width: 304px;
-display: flex;
-box-sizing: border-box;
-justify-content: space-between;
-align-items: flex-start;
+  justify-content: space-between;
+  height: 32px;
+  width: 304px;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: flex-start;
 `;
 export const ButtonsButton2 = styled.div`
-height: 22px;
-width: 123px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
-border-radius: 2px;
+  height: 22px;
+  width: 123px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 2px;
 `;
 export const Content1 = styled.div`
-height: 22px;
-width: 123px;
-display: flex;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
+  height: 22px;
+  width: 123px;
+  display: flex;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const Button = styled(GenericText)`
-        font-size: 14px;
-        color: #7447F9;
-        margin: 0px;
-      width: 124px;
+  font-size: 14px;
+  color: #7447f9;
+  margin: 0px;
+  width: 124px;
 `;
 
 export const ButtonsButton3 = styled.div`
-height: 32px;
-width: 67px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
-border-radius: 2px;
-  padding: 5.0px 16.0px 5.0px 16.0px;
-  background-color: #7447F9;
+  height: 32px;
+  width: 67px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 2px;
+  padding: 5px 16px 5px 16px;
+  background-color: #7447f9;
 `;
 export const Content2 = styled.div`
-height: 22px;
-width: 35px;
-display: flex;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
+  height: 22px;
+  width: 35px;
+  display: flex;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const Button1 = styled(GenericText)`
-      font-size: 14px;
-      color: #FFFFFF;
-      margin: 0px;
-    width: 36px;
+  font-size: 14px;
+  color: #ffffff;
+  margin: 0px;
+  width: 36px;
 `;
 
 export const CreateaAccount = styled.div`
-height: 500px;
-width: 400px;
-display: flex;
-box-sizing: border-box;
-justify-content: center;
-align-items: flex-end;
-padding: 48.0px 48.0px 158.0px 48.0px;
-  background-color: #F7F1FF;
+  height: 500px;
+  width: 400px;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 48px 48px 158px 48px;
+  background-color: #f7f1ff;
 `;
 export const Content3 = styled.div`
-height: 182px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 40.0px;
-justify-content: flex-start;
-align-items: flex-start;
+  height: 182px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 40px;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const Logo = styled.div`
-height: 24px;
-width: 126px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-justify-content: flex-start;
-align-items: flex-start;
+  height: 24px;
+  width: 126px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const Vector1484054909088 = styled.div`
-height: 24px;
-width: 126px;
-box-sizing: border-box;
+  height: 24px;
+  width: 126px;
+  box-sizing: border-box;
   background: url(${Vector1484054909088Image});
   background-repeat: no-repeat;
 `;
 export const Text1 = styled.div`
-height: 46px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-justify-content: flex-start;
-align-items: flex-start;
+  height: 46px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const Comecreatethefuture = styled(GenericText)`
-      font-weight: 500;
-      font-size: 16px;
-      margin: 0px;
-    width: 305px;
+  font-weight: 500;
+  font-size: 16px;
+  margin: 0px;
+  width: 305px;
 `;
 
 export const TextComponentjoin = styled(GenericText)`
-    font-size: 14px;
-    margin: 0px;
+  font-size: 14px;
+  margin: 0px;
   width: 189px;
 `;
 
 export const Actions1 = styled.div`
-height: 32px;
-width: 304px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: flex-start;
+  height: 32px;
+  width: 304px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 export const ButtonsButton4 = styled.div`
-height: 32px;
-width: 73px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
-border-radius: 2px;
-  padding: 5.0px 16.0px 5.0px 16.0px;
-  background-color: #7447F9;
+  height: 32px;
+  width: 73px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 2px;
+  padding: 5px 16px 5px 16px;
+  background-color: #7447f9;
 `;
 export const Content4 = styled.div`
-height: 22px;
-width: 41px;
-display: flex;
-box-sizing: border-box;
-gap: 8.0px;
-justify-content: flex-start;
-align-items: center;
+  height: 22px;
+  width: 41px;
+  display: flex;
+  box-sizing: border-box;
+  gap: 8px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 export const Button2 = styled(GenericText)`
-      font-size: 14px;
-      color: #FFFFFF;
-      margin: 0px;
-    width: 42px;
+  font-size: 14px;
+  color: #ffffff;
+  margin: 0px;
+  width: 42px;
 `;
-
