fb6cdda2f7a0914cace2fe25976951284cc84bac
kairo <kairogyn@gmail.com>
2022-06-14 15:47:07 -0300

    Merge branch 'design2code-generated-code'

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -57,6 +57,7 @@ const LoginPage = () => {
               label={<Typography.Text ellipsis>E-mail</Typography.Text>}
               name="email"
             >
+              <div>kairo</div>
               <GenericInput placeholder="E-mail" />
             </GenericFormItem>
             <GenericFormItem
