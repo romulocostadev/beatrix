40186fe0236b76434a2f85688a7dccad50d76ec9
kairo <kairogyn@gmail.com>
2022-06-15 11:40:51 -0300

    pull generated code

--- src/pages/HomeContent/components/Content01/index.tsx
+++ src/pages/HomeContent/components/Content01/index.tsx
@@ -1,137 +1,89 @@
 import React from 'react';
 
-
-
 import {
-Content011,
-LessonsCard1,
-Lessons2,
-Lessons1,
-Bar,
-Logo3,
-Frame9,
-ButtonsButton6,
-Stage,
-Creatingdata,
-Howtocreateadataworkflow,
-ButtonsButton7,
-Solutions1,
-Title4,
-Solutions,
-ButtonsButton8,
-Sorter,
-Sortby,
-Name,
-Lastmodified,
-MySolutions,
-Solution02,
-Logo4,
-Logo5,
-Veiling,
-Apr1820221356,
-Solution03,
-Logo6,
-Logo7,
-Iris,
-Apr1720221356,
+  Content011,
+  LessonsCard1,
+  Lessons2,
+  Lessons1,
+  Bar,
+  Logo3,
+  Frame9,
+  ButtonsButton6,
+  Stage,
+  Creatingdata,
+  Howtocreateadataworkflow,
+  ButtonsButton7,
+  Solutions1,
+  Title4,
+  Solutions,
+  ButtonsButton8,
+  Sorter,
+  Sortby,
+  Name,
+  Lastmodified,
+  MySolutions,
+  Solution02,
+  Logo4,
+  Logo5,
+  Veiling,
+  Apr1820221356,
+  Solution03,
+  Logo6,
+  Logo7,
+  Iris,
+  Apr1720221356,
 } from './styles';
 
 const Content01Page = () => {
+  return (
+    <Content011>
+      <LessonsCard1>
+        <Lessons2>
+          <Lessons1>Lessons</Lessons1>
+          <Bar>
+            <Logo3>
+              <Frame9 />
+            </Logo3>
+          </Bar>
+          <ButtonsButton6 type="link">Start lesson</ButtonsButton6>
+        </Lessons2>
+        <Stage>
+          <Creatingdata>Creating data</Creatingdata>
+          <Howtocreateadataworkflow>
+            How to create a data workflow.
+          </Howtocreateadataworkflow>
+        </Stage>
+        <ButtonsButton7 type="link">View all lessons</ButtonsButton7>
+      </LessonsCard1>
+      <Solutions1>
+        <Title4>
+          <Solutions>Solutions</Solutions>
+          <ButtonsButton8 type="primary">New solution</ButtonsButton8>
+        </Title4>
+        <Sorter>
+          <Sortby>Sort by</Sortby>
+          <Name>name</Name>
+          <Lastmodified>last modified</Lastmodified>
+        </Sorter>
+        <MySolutions>
+          <Solution02>
+            <Logo4 />
+            <Logo5>
+              <Veiling>Veiling</Veiling>
+              <Apr1820221356>Apr 18, 2022, 13:56</Apr1820221356>
+            </Logo5>
+          </Solution02>
+          <Solution03>
+            <Logo6 />
+            <Logo7>
+              <Iris>Iris</Iris>
+              <Apr1720221356>Apr 17, 2022, 13:56</Apr1720221356>
+            </Logo7>
+          </Solution03>
+        </MySolutions>
+      </Solutions1>
+    </Content011>
+  );
+};
 
-
-
-
-
-
-
-
-
-return (
-        <Content011    >
-  <LessonsCard1    >
-    <Lessons2    >
-      <Lessons1 >
-        Lessons
-      </Lessons1>
-      <Bar    >
-        <Logo3    >
-          <Frame9    >
-          </Frame9>
-        </Logo3>
-      </Bar>
-      <ButtonsButton6  
-        type='link'
-        >
-          Start lesson
-      </ButtonsButton6>
-    </Lessons2>
-    <Stage    >
-      <Creatingdata >
-        Creating data
-      </Creatingdata>
-      <Howtocreateadataworkflow >
-        How to create a data workflow.
-      </Howtocreateadataworkflow>
-    </Stage>
-    <ButtonsButton7  
-      type='link'
-      >
-        View all lessons
-    </ButtonsButton7>
-  </LessonsCard1>
-  <Solutions1    >
-    <Title4    >
-      <Solutions >
-        Solutions
-      </Solutions>
-      <ButtonsButton8  
-        type='primary'
-        >
-          New solution
-      </ButtonsButton8>
-    </Title4>
-    <Sorter    >
-      <Sortby >
-        Sort by
-      </Sortby>
-      <Name >
-        name
-      </Name>
-      <Lastmodified >
-        last modified
-      </Lastmodified>
-    </Sorter>
-    <MySolutions    >
-      <Solution02    >
-        <Logo4    >
-        </Logo4>
-        <Logo5    >
-          <Veiling >
-            Veiling
-          </Veiling>
-          <Apr1820221356 >
-            Apr 18, 2022, 13:56
-          </Apr1820221356>
-        </Logo5>
-      </Solution02>
-      <Solution03    >
-        <Logo6    >
-        </Logo6>
-        <Logo7    >
-          <Iris >
-            Iris
-          </Iris>
-          <Apr1720221356 >
-            Apr 17, 2022, 13:56
-          </Apr1720221356>
-        </Logo7>
-      </Solution03>
-    </MySolutions>
-  </Solutions1>
-</Content011>
-
-    )
-
-}
-
-export default Content01Page
+export default Content01Page;
