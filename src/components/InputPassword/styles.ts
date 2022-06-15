58238e062dc9c4baf415611f40d7b6f76b4cd808
kairo <kairogyn@gmail.com>
2022-06-15 13:18:26 -0300

    zerando

--- src/components/InputPassword/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { Input } from 'antd';
-
-export const GenericInputPasswordStyle = styled(Input.Password)`
-  width: 304px;
-  position: static;
-`;
--- src/components/Text/index.tsx
+++
@@ -1,7 +0,0 @@
-import { GenericTextStyle } from './styles';
-
-const GenericText = ({ ...props }: any) => {
-  return <GenericTextStyle {...props} />;
-};
-
-export default GenericText;
