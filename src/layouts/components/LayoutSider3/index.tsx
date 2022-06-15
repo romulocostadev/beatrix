8dd59ca753654b28436ab0ad43e5dec8be6842b8
kairo <kairogyn@gmail.com>
2022-06-15 11:25:34 -0300

    pull generated code

--- src/layouts/components/LayoutSider3/index.tsx
+++ src/layouts/components/LayoutSider3/index.tsx
@@ -1,126 +1,96 @@
 import React from 'react';
 
-import {PlusOutlined} from '@ant-design/icons' 
-
+import { PlusOutlined } from '@ant-design/icons';
 
 import {
-LayoutSider1,
-MenuGroup1,
-Layers,
-TreeItem,
-Frame1,
-IconOutlinedCaretDown,
-Icon2,
-IconOutlinedFolderOpen,
-ItemWrapper,
-Database,
-ButtonsButton2,
-TreeItem1,
-Frame11,
-IconOutlinedCaretDown1,
-Icon3,
-IconOutlinedFolderOpen1,
-ItemWrapper1,
-Database1,
-ButtonsButton14,
-Content11,
-IconOutlinedPlus,
-Button,
-TreeItem2,
-Frame12,
-IconOutlinedCaretDown2,
-Icon4,
-IconOutlinedFolderOpen2,
-ItemWrapper2,
-Database2,
-ButtonsButton15,
-Content12,
-IconOutlinedPlus1,
-Button,
+  LayoutSider1,
+  MenuGroup1,
+  Layers,
+  TreeItem,
+  Frame1,
+  IconOutlinedCaretDown,
+  Icon2,
+  IconOutlinedFolderOpen,
+  ItemWrapper,
+  Database,
+  ButtonsButton2,
+  TreeItem1,
+  Frame11,
+  IconOutlinedCaretDown1,
+  Icon3,
+  IconOutlinedFolderOpen1,
+  ItemWrapper1,
+  Database1,
+  ButtonsButton14,
+  Content11,
+  IconOutlinedPlus,
+  Button,
+  TreeItem2,
+  Frame12,
+  IconOutlinedCaretDown2,
+  Icon4,
+  IconOutlinedFolderOpen2,
+  ItemWrapper2,
+  Database2,
+  ButtonsButton15,
+  Content12,
+  IconOutlinedPlus1,
+  Button,
 } from './styles';
 
 const LayoutSiderPage = () => {
+  return (
+    <LayoutSider1>
+      <MenuGroup1>
+        <Layers>
+          <TreeItem>
+            <Frame1>
+              <IconOutlinedCaretDown />
+            </Frame1>
+            <Icon2>
+              <IconOutlinedFolderOpen />
+            </Icon2>
+            <ItemWrapper>
+              <Database>SunChemical</Database>
+            </ItemWrapper>
+            <ButtonsButton2 type="text" icon={<PlusOutlined />} />
+          </TreeItem>
+          <TreeItem1>
+            <Frame11>
+              <IconOutlinedCaretDown1 />
+            </Frame11>
+            <Icon3>
+              <IconOutlinedFolderOpen1 />
+            </Icon3>
+            <ItemWrapper1>
+              <Database1>Database</Database1>
+            </ItemWrapper1>
+            <ButtonsButton14>
+              <Content11>
+                <IconOutlinedPlus />
+              </Content11>
+            </ButtonsButton14>
+          </TreeItem1>
+          <TreeItem2>
+            <Frame12>
+              <IconOutlinedCaretDown2 />
+            </Frame12>
+            <Icon4>
+              <IconOutlinedFolderOpen2 />
+            </Icon4>
+            <ItemWrapper2>
+              <Database2>Services</Database2>
+            </ItemWrapper2>
+            <ButtonsButton15>
+              <Content12>
+                <IconOutlinedPlus1 />
+              </Content12>
+            </ButtonsButton15>
+          </TreeItem2>
+        </Layers>
+      </MenuGroup1>
+    </LayoutSider1>
+  );
+};
 
-
-
-
-
-
-
-
-
-return (
-        <LayoutSider1    >
-  <MenuGroup1    >
-    <Layers    >
-      <TreeItem    >
-        <Frame1    >
-          <IconOutlinedCaretDown    >
-          </IconOutlinedCaretDown>
-        </Frame1>
-        <Icon2    >
-          <IconOutlinedFolderOpen    >
-          </IconOutlinedFolderOpen>
-        </Icon2>
-        <ItemWrapper    >
-          <Database >
-            SunChemical
-          </Database>
-        </ItemWrapper>
-        <ButtonsButton2  
-          type='text'
-          icon={<PlusOutlined/>}
-          >
-        </ButtonsButton2>
-      </TreeItem>
-      <TreeItem1    >
-        <Frame11    >
-          <IconOutlinedCaretDown1    >
-          </IconOutlinedCaretDown1>
-        </Frame11>
-        <Icon3    >
-          <IconOutlinedFolderOpen1    >
-          </IconOutlinedFolderOpen1>
-        </Icon3>
-        <ItemWrapper1    >
-          <Database1 >
-            Database
-          </Database1>
-        </ItemWrapper1>
-        <ButtonsButton14    >
-          <Content11    >
-            <IconOutlinedPlus    >
-            </IconOutlinedPlus>
-          </Content11>
-        </ButtonsButton14>
-      </TreeItem1>
-      <TreeItem2    >
-        <Frame12    >
-          <IconOutlinedCaretDown2    >
-          </IconOutlinedCaretDown2>
-        </Frame12>
-        <Icon4    >
-          <IconOutlinedFolderOpen2    >
-          </IconOutlinedFolderOpen2>
-        </Icon4>
-        <ItemWrapper2    >
-          <Database2 >
-            Services
-          </Database2>
-        </ItemWrapper2>
-        <ButtonsButton15    >
-          <Content12    >
-            <IconOutlinedPlus1    >
-            </IconOutlinedPlus1>
-          </Content12>
-        </ButtonsButton15>
-      </TreeItem2>
-    </Layers>
-  </MenuGroup1>
-</LayoutSider1>
-
-    )
-
-}
-
-export default LayoutSiderPage
+export default LayoutSiderPage;
--- src/pages/CreateAccount/index.tsx
+++ src/pages/CreateAccount/index.tsx
@@ -1,118 +1,95 @@
 import React from 'react';
 
-import {useNavigate} from 'react-router-dom';
-import GenericFormItem from '../../components/FormItem' 
+import { useNavigate } from 'react-router-dom';
 import { Typography } from 'antd';
-import GenericInput from '../../components/Input' 
-
+import GenericFormItem from '../../components/FormItem';
+import GenericInput from '../../components/Input';
 
 import {
-CreateAccount,
-Login2,
-Content5,
-Title1,
-TextComponentcreating,
-ViceriForm1,
-Actions2,
-ButtonsButton,
-CreateaAccount1,
-Content6,
-Logo1,
-Vector14840549090881,
-Text2,
-Alreadyhaveanaccount,
-Wearewaitingforyou,
-Actions3,
-ButtonsButton12,
-Content7,
-Button3,
+  CreateAccount,
+  Login2,
+  Content5,
+  Title1,
+  TextComponentcreating,
+  ViceriForm1,
+  Actions2,
+  ButtonsButton,
+  CreateaAccount1,
+  Content6,
+  Logo1,
+  Vector14840549090881,
+  Text2,
+  Alreadyhaveanaccount,
+  Wearewaitingforyou,
+  Actions3,
+  ButtonsButton12,
+  Content7,
+  Button3,
 } from './styles';
 
 const CreateAccountPage = () => {
+  let navigate = useNavigate();
 
+  const navigateToLogin = () => {
+    navigate('/login');
+  };
 
+  return (
+    <CreateAccount>
+      <Login2>
+        <Content5>
+          <Title1>
+            <TextComponentcreating>
+              Creating an account to the future!
+            </TextComponentcreating>
+          </Title1>
+          <ViceriForm1 onClick={navigateToLogin}>
+            <GenericFormItem
+              label={<Typography.Text ellipsis>E-mail</Typography.Text>}
+              name="email"
+            >
+              <GenericInput placeholder="E-mail" />
+            </GenericFormItem>
+            <GenericFormItem
+              label={<Typography.Text ellipsis>E-mail</Typography.Text>}
+              name="email"
+            >
+              <GenericInput placeholder="E-mail" />
+            </GenericFormItem>
+            <GenericFormItem
+              label={<Typography.Text ellipsis>E-mail</Typography.Text>}
+              name="email"
+            >
+              <GenericInput placeholder="E-mail" />
+            </GenericFormItem>
+          </ViceriForm1>
+          <Actions2>
+            <ButtonsButton type="primary">Create</ButtonsButton>
+          </Actions2>
+        </Content5>
+      </Login2>
+      <CreateaAccount1>
+        <Content6>
+          <Logo1>
+            <Vector14840549090881 />
+          </Logo1>
+          <Text2>
+            <Alreadyhaveanaccount>
+              Already have an account?
+            </Alreadyhaveanaccount>
+            <Wearewaitingforyou>We are waiting for you.</Wearewaitingforyou>
+          </Text2>
+          <Actions3>
+            <ButtonsButton12>
+              <Content7>
+                <Button3>Login</Button3>
+              </Content7>
+            </ButtonsButton12>
+          </Actions3>
+        </Content6>
+      </CreateaAccount1>
+    </CreateAccount>
+  );
+};
 
-
-let navigate = useNavigate()
-
-const navigateToLogin = () => {navigate('/login')};
-
-
-
-
-return (
-        <CreateAccount    >
-  <Login2    >
-    <Content5    >
-      <Title1    >
-        <TextComponentcreating >
-          Creating an account to the future!
-        </TextComponentcreating>
-      </Title1>
-      <ViceriForm1   onClick = {navigateToLogin} >
-        <GenericFormItem
-        label={<Typography.Text ellipsis>E-mail</Typography.Text>}
-        name='email'
-        >
-          <GenericInput
-        placeholder='E-mail'
-          />
-        </GenericFormItem>
-        <GenericFormItem
-        label={<Typography.Text ellipsis>E-mail</Typography.Text>}
-        name='email'
-        >
-          <GenericInput
-        placeholder='E-mail'
-          />
-        </GenericFormItem>
-        <GenericFormItem
-        label={<Typography.Text ellipsis>E-mail</Typography.Text>}
-        name='email'
-        >
-          <GenericInput
-        placeholder='E-mail'
-          />
-        </GenericFormItem>
-      </ViceriForm1>
-      <Actions2    >
-        <ButtonsButton  
-          type='primary'
-          >
-            Create
-        </ButtonsButton>
-      </Actions2>
-    </Content5>
-  </Login2>
-  <CreateaAccount1    >
-    <Content6    >
-      <Logo1    >
-        <Vector14840549090881    >
-        </Vector14840549090881>
-      </Logo1>
-      <Text2    >
-        <Alreadyhaveanaccount >
-          Already have an account?
-        </Alreadyhaveanaccount>
-        <Wearewaitingforyou >
-          We are waiting for you.
-        </Wearewaitingforyou>
-      </Text2>
-      <Actions3    >
-        <ButtonsButton12    >
-          <Content7    >
-            <Button3 >
-              Login
-            </Button3>
-          </Content7>
-        </ButtonsButton12>
-      </Actions3>
-    </Content6>
-  </CreateaAccount1>
-</CreateAccount>
-
-    )
-
-}
-
-export default CreateAccountPage
+export default CreateAccountPage;
